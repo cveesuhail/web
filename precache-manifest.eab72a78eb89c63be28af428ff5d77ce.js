387fdee321828f2b73e2693f02be4e0a0b188b70
cvsuhail <cvsuhail.ckd@gmail.com>
2022-04-27 16:11:33 +0530

    Updates

---
+++ precache-manifest.eab72a78eb89c63be28af428ff5d77ce.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e64495ab35902018bf2497cafff998",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "f03b36c39cab0ef49c00",
+    "url": "/web/static/css/main.9d7779eb.chunk.css"
+  },
+  {
+    "revision": "0c03a9d145115e79901b",
+    "url": "/web/static/js/2.a6879ec0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/web/static/js/2.a6879ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03b36c39cab0ef49c00",
+    "url": "/web/static/js/main.9883056d.chunk.js"
+  },
+  {
+    "revision": "8611874e5ec2c1d8dab0",
+    "url": "/web/static/js/runtime-main.33aab821.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/web/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/web/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/web/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/web/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/web/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/web/static/media/d.9c1952ca.png"
+  },
+  {
+    "revision": "8290266a5fb402a34b96f890bbdb2d60",
+    "url": "/web/static/media/instagram.8290266a.png"
+  },
+  {
+    "revision": "4e9d0776e39aea9bd9bb16947e4a1c03",
+    "url": "/web/static/media/matt.4e9d0776.png"
+  },
+  {
+    "revision": "8b357b47f00b0b898a8ba58203557582",
+    "url": "/web/static/media/whatsapp.8b357b47.png"
+  }
+]);
